24f2029336bb292251f02b19effae5d929020865
Nico-Mayer <nico.m0623@gmail.com>
2026-07-04 13:37:21 +0200

    refactor: remove legacy codebase and dependencies
    Strip all cmd, internal packages, and Go dependencies for fresh rebuild.

--- internal/model/theme.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type ThemeInfo struct {
-	Name             string            `json:"name"`
-	Appearance       string            `json:"appearance"`
-	WallpaperSources []string          `json:"wallpaper-sources"`
-	Overrides        map[string]string `json:"overrides"`
-}
--- internal/random/random.go
+++
@@ -1,7 +0,0 @@
-package random
-
-import "math/rand/v2"
-
-func Element[T any](slice []T) T {
-	return slice[rand.IntN(len(slice))]
-}
