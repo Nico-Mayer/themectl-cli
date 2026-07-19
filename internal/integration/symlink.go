89605f5b1c40bd0dc30c693833f39fa19594823b
Nico-Mayer <nico.m0623@gmail.com>
2026-07-19 18:34:17 +0200

    refactor(integrations): consolidate nvim, eza, yazi into generic SymlinkIntegration

--- internal/integration/symlink.go
+++ internal/integration/symlink.go
@@ -5,7 +5,25 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+
+	"github.com/Nico-Mayer/themectl/internal/theme"
 )
+
+type SymlinkIntegration struct {
+	IntegrationName string
+	SourceFile      string
+	Target          string
+}
+
+func (s SymlinkIntegration) Name() string { return s.IntegrationName }
+
+func (s SymlinkIntegration) Apply(theme.Resolved) error {
+	return symlink(s.SourceFile, s.Target)
+}
+
+func (s SymlinkIntegration) Check() error {
+	return checkFileExists(s.IntegrationName+" theme asset", s.SourceFile)
+}
 
 func symlink(source, target string) error {
 	if _, err := os.Stat(source); err != nil {
